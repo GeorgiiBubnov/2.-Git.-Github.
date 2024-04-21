245281f8cb51518446de39d119eb7d26e192ca58
Георгий Бубнов <149017290+GeorgiiBubnov@users.noreply.github.com>
2024-04-21 20:11:21 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    private static String msg = "Hello qa.guru!!!!";
+    private static String msg = "Hello qa.guru!!!!+4564";
 
     public static void main(String... args) {
         System.out.println(msg);
